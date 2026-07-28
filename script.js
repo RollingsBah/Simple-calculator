19c2065f31ea89c6819268f7db8a756166629563
Sister Vivian <gauchohazard257@gmail.com>
2026-07-28 17:03:33 +0100

    update calculator logic

--- script.js
+++ script.js
@@ -5,48 +5,111 @@ const display = document.getElementById("display");
 function appendValue(value){
     display.value += value;
 }
+
 // Calculates the answer
 function calculate(){
+
     let expression = display.value;
 
+    // Combination
+    if(expression.includes("C")){
+
+        let numbers = expression.split("C");
+
+        let n = Number(numbers[0]);
+        let r = Number(numbers[1]);
+
+        display.value = combination(n, r);
+
+    }
+
     // Addition
-    if(expression.includes("+")){
+    else if(expression.includes("+")){
+
         let numbers = expression.split("+");
         let first = Number(numbers[0]);
         let second = Number(numbers[1]);
+
         display.value = first + second;
+
     }
+
     // Subtraction
     else if(expression.includes("-")){
+
         let numbers = expression.split("-");
         let first = Number(numbers[0]);
         let second = Number(numbers[1]);
+
         display.value = first - second;
+
     }
 
     // Multiplication
     else if(expression.includes("*")){
+
         let numbers = expression.split("*");
         let first = Number(numbers[0]);
         let second = Number(numbers[1]);
+
         display.value = first * second;
+
     }
 
     // Division
     else if(expression.includes("/")){
+
         let numbers = expression.split("/");
         let first = Number(numbers[0]);
         let second = Number(numbers[1]);
+
         if(second == 0){
+
             display.value = "Error";
+
         }
         else{
+
             display.value = first / second;
+
         }
+
     }
 
     // Invalid expression
     else{
+
         display.value = "Error";
+
     }
+
+}
+
+
+// Calculates factorial
+function factorial(number){
+
+    let result = 1;
+
+    for(let i = 1; i <= number; i++){
+
+        result = result * i;
+
+    }
+
+    return result;
+
+}
+
+
+// Calculates combination
+function combination(n, r){
+
+    let answer;
+
+    answer = factorial(n) /
+            (factorial(r) * factorial(n-r));
+
+    return answer;
+
 }
