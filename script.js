5c1c1072c8cfc579eb18a2c3c88a416546c6c49a
Sister Vivian <gauchohazard257@gmail.com>
2026-07-29 09:48:22 +0100

    update calculator and html

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function appendValue(value){
 // Calculates the answer
 function calculate(){
     let expression = display.value;
-    let expression = display.value;
 
     // Combination
     if(expression.includes("C")){
@@ -75,43 +74,6 @@ function calculate(){
 
     }
 
-    // Invalid expression
-    else{
-    // Addition
-    if(expression.includes("+")){
-        let numbers = expression.split("+");
-        let first = Number(numbers[0]);
-        let second = Number(numbers[1]);
-        display.value = first + second;
-    }
-    // Subtraction
-    else if(expression.includes("-")){
-        let numbers = expression.split("-");
-        let first = Number(numbers[0]);
-        let second = Number(numbers[1]);
-        display.value = first - second;
-    }
-
-    // Multiplication
-    else if(expression.includes("*")){
-        let numbers = expression.split("*");
-        let first = Number(numbers[0]);
-        let second = Number(numbers[1]);
-        display.value = first * second;
-    }
-
-    // Division
-    else if(expression.includes("/")){
-        let numbers = expression.split("/");
-        let first = Number(numbers[0]);
-        let second = Number(numbers[1]);
-        if(second == 0){
-            display.value = "Error";
-        }
-        else{
-            display.value = first / second;
-        }
-    }
 
     // Invalid expression
     else{
@@ -147,5 +109,4 @@ function combination(n, r){
 
     return answer;
 
-}
 }
